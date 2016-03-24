f8715a7dc9b3336966fba3a8d5ff47c022a31b87
seokgyo <seokgyo@gmail.com>
2016-03-24 23:40:12 +0900

    update stock.

--- src/order.js
+++ src/order.js
@@ -14,3 +14,7 @@ export function loadBrandOrders(auth, { brandId, start, end }) {
 export function loadBrandPendingOrders(auth, { brandId }) {
   return request(auth, 'get', `/api/v1/brands/${brandId}/orders/pending`);
 }
+
+export function updateStock(auth, { orderProductId, count }) {
+  return request(auth, 'put', `/api/v1/order_products/${orderProductId}/stock`, { count });
+}
