cffdd2fbd2e1c7303e733344a7b233de54a50048
seokgyo <seokgyo@gmail.com>
2016-03-25 15:41:17 +0900

    uses 'qs' to generate query string.

--- src/order.js
+++ src/order.js
@@ -1,13 +1,14 @@
 // Copyright (C) 2016 Goom Inc. All rights reserved.
 
 import { request } from './util';
+import qs from 'qs';
 
 export function loadBrandOrderStats(auth, { brandId }) {
   return request(auth, 'get', `/api/v1/brands/${brandId}/orders/stats`);
 }
 
 export function loadBrandOrders(auth, { brandId, start, end }) {
-  const query = $.param({ start, end });
+  const query = qs.stringify({ start, end });
   return request(auth, 'get', `/api/v1/brands/${brandId}/orders?${query}`);
 }
 
