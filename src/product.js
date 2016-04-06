9d61bf9e9fe0b9160d049d5ecc1f6b82acaa096c
seokgyo <seokgyo@gmail.com>
2016-04-06 17:17:34 +0900

    loadProduct.

--- src/product.js
+++ src/product.js
@@ -3,6 +3,10 @@
 import { request } from './util';
 import qs from 'qs';
 
+export function loadProduct(auth, { productId }) {
+  return request(auth, 'get', `/api/v1/products/${productId}`);
+}
+
 export function loadBrandProducts(auth, { brandId, offset, limit }) {
   const query = qs.stringify({ offset, limit });
   return request(auth, 'get', `/api/v1/brands/${brandId}/products?${query}`);
