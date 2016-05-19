ab50f66c90e8bc8dfa4f102a2c11a028ad19da2d
seokgyo <seokgyo@gmail.com>
2016-05-19 14:56:43 +0900

    add readyToPickUp and deleteStock

--- src/order.js
+++ src/order.js
@@ -20,10 +20,18 @@ export function updateBrandOrderStatus(auth, { brandId, orderId, from, to }) {
   return request(auth, 'put', `/api/v1/brands/${brandId}/orders/${orderId}/status`, { from, to });
 }
 
+export function brandOrderReadyToPickUp(auth, { brandId, orderId, orderProducts }) {
+  return request(auth, 'put', `/api/v1/brands/${brandId}/orders/${orderId}/ready2pickup`, { orderProducts });
+}
+
 export function createOrderProductLog(auth, { orderProductId, ...params }) {
   return request(auth, 'post', `/api/v1/order_products/${orderProductId}/logs`, params);
 }
 
 export function updateOrderProductStock(auth, { orderProductId, quantity, reason }) {
   return request(auth, 'put', `/api/v1/order_products/${orderProductId}/stock`, { quantity, reason });
+}
+
+export function deleteOrderProductStock(auth, { orderProductId }) {
+  return request(auth, 'delete', `/api/v1/order_products/${orderProductId}/stock`);
 }
