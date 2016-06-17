8d0f5ac1d65af72711c6b62b7134cfdfc580c0ab
seokgyo <seokgyo@gmail.com>
2016-06-17 15:37:56 +0900

    pick up.

--- src/uncle.js
+++ src/uncle.js
@@ -5,3 +5,11 @@ import { request } from './util';
 export function loadUncleOrders(auth, { date }) {
   return request(auth, 'get', `/api/v1/uncle/orders/${date}`);
 }
+
+export function unclePickUp(auth, { brandId, orderId, orderProducts }) {
+  return request(auth, 'put', `/api/v1/brands/${brandId}/orders/${orderId}/pick_up`, { orderProducts });
+}
+
+export function uncleCancelPickUp(auth, { brandId, orderId, orderProducts }) {
+  return request(auth, 'delete', `/api/v1/brands/${brandId}/orders/${orderId}/pick_up`, { orderProducts });
+}
